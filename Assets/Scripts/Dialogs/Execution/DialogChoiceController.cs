8fc5e4c20c05a6e9e30597d2686341edb1ab59f3
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2024-01-06 21:29:15 +0100

    Dynamic dialog option buttons

--- Assets/Scripts/Dialogs/Execution/DialogChoiceController.cs
+++ Assets/Scripts/Dialogs/Execution/DialogChoiceController.cs
@@ -9,15 +9,103 @@ namespace Dialogs
 {
     public class DialogChoiceController : MonoBehaviour
     {
+        [SerializeField] GameObject guiContainer;
+        [SerializeField] GameObject chosenOptionTemplate;
+        [SerializeField] GameObject unchosenOptionTemplate;
+        [SerializeField] float optionDistances = 50f;
+
         private int _currentSelectedOptionIdx = 0;
         private string[] _playerOptions = null;
         private Action<int> _onOptionChosen = null;
+
+        private int _currentSelectedOptionInGui = 0;
+        private GameObject[] _playerOptionsGui = null;
 
         public void DisplayPlayerOptions(string[] options, Action<int> onOptionChosen)
         {
             _currentSelectedOptionIdx = 0;
             _playerOptions = options;
             _onOptionChosen = onOptionChosen;
+
+            guiContainer.SetActive(true);
+            AdjustContainerHeight();
+            CreateGuiOptions();
+        }
+
+        private void CreateGuiOptions()
+        {
+            if (_playerOptions.Length < 1)
+                return;
+
+            _playerOptionsGui = new GameObject[_playerOptions.Length];
+
+            // Stwórz jedną wybraną opcję
+            GameObject chosenOption = GameObject.Instantiate(chosenOptionTemplate, guiContainer.transform);
+            _playerOptionsGui[0] = chosenOption;
+            chosenOption.SetActive(true);
+
+            // Stwórz (n - 1) niewybranych opcji
+            for(int i = 1; i < _playerOptions.Length; i++)
+            {
+                GameObject option = GameObject.Instantiate(unchosenOptionTemplate, guiContainer.transform);
+                RectTransform optionTransform = option.GetComponent<RectTransform>();
+                // Ustaw pozycję
+                optionTransform.anchoredPosition = new Vector2(optionTransform.anchoredPosition.x, i * optionDistances);
+
+                _playerOptionsGui[i] = option;
+                option.SetActive(true);
+            }
+
+            _currentSelectedOptionInGui = 0;
+
+            UpdateOptionsText();
+            UpdateOptionsSelection();
+        }
+
+        private void UpdateOptionsText()
+        {
+            for(int i = 0; i < _playerOptions.Length; i++)
+            {
+                GameObject guiOption = _playerOptionsGui[i];
+                string textToInsert = _playerOptions[i];
+
+                UpdateOptionText(guiOption, textToInsert);
+            }
+        }
+
+        private void UpdateOptionText(GameObject guiOption, string newText)
+        {
+            guiOption.GetComponentInChildren<TMPro.TMP_Text>().text = newText;
+        }
+
+        private void AdjustContainerHeight()
+        {
+            RectTransform containerTransform = guiContainer.GetComponent<RectTransform>();
+            containerTransform.sizeDelta = new Vector2(containerTransform.sizeDelta.x, _playerOptions.Length * optionDistances);
+        }
+
+        private void UpdateOptionsSelection()
+        {
+            if (_currentSelectedOptionInGui == _currentSelectedOptionIdx)
+                return; // Up-to-date
+
+            // Zamień pozycje i teksty ostatnio wybranej opcji z nowowybraną opcją
+            // Zamień pozycje w tabeli
+        }
+
+        private void DestroyGuiOptions()
+        {
+            foreach(GameObject option in _playerOptionsGui)
+            {
+                GameObject.Destroy(option);
+            }
+
+            _playerOptionsGui = null;
+        }
+
+        private void Awake()
+        {
+            guiContainer.SetActive(false);
         }
 
         private void Update()
@@ -71,6 +159,7 @@ namespace Dialogs
             _onOptionChosen?.Invoke(_currentSelectedOptionIdx);
 
             _playerOptions = null;
+            guiContainer.SetActive(false);
         }
     }
 }
