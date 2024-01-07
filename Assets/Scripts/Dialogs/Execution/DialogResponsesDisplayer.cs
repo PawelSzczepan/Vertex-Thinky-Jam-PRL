e9b9760c12c6768bdda649da05a46ad0614909a5
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2024-01-07 18:39:29 +0100

    Slow text displaying

--- Assets/Scripts/Dialogs/Execution/DialogResponsesDisplayer.cs
+++ Assets/Scripts/Dialogs/Execution/DialogResponsesDisplayer.cs
@@ -14,31 +14,48 @@ namespace Dialogs
         [SerializeField] TMP_Text newestDialogText;
         [SerializeField] TMP_Text dialogHistory;
         [SerializeField] int historySize = 2;
+        [SerializeField] float timeBetweenLetters = 0.1f;
+        [SerializeField] float timeAfterFinish = 1f;
 
-        private Queue<string> historyTexts = new Queue<string>();
+        private Queue<string> _historyTexts = new Queue<string>();
+        private string _newestActorLabel = "";
+        private bool _isTextConstructionInProgress = false;
+        private SlowTextConstructor _textUpdater;
+        private Action _onWorkDone = null;
 
-        public void DisplayDialogText(string text, Action onWorkDone)
+        public void DisplayDialogText(string actorLabel, string text, Action onWorkDone)
         {
             AddToHistory(newestDialogText.text);
-            // TODO: powolne wypisywanie tekstu
-            newestDialogText.text = text;
-            StartCoroutine(CooldownDialogText(onWorkDone)); // Testowe spowolnienie wyświetlania następnego tekstu
+
+            // Display the actor label now, add the rest progressively
+            _newestActorLabel = $"[{actorLabel}] ";
+            newestDialogText.text = _newestActorLabel;
+            _isTextConstructionInProgress = true;
+            _onWorkDone = onWorkDone;
+
+            _textUpdater.RequestText(text);
         }
 
-        private IEnumerator CooldownDialogText(Action onFinished)
+        private void OnTextFinished()
         {
-            yield return new WaitForSeconds(1f);
-            onFinished?.Invoke();
+            _isTextConstructionInProgress = false;
+            StartCoroutine(InvokeWorkDoneAfterTime(timeAfterFinish));
+        }
+
+        private IEnumerator InvokeWorkDoneAfterTime(float time)
+        {
+            yield return new WaitForSeconds(time);
+            _onWorkDone?.Invoke();
         }
 
         private void AddToHistory(string text)
         {
-            while (historyTexts.Count >= historySize)
+            while (_historyTexts.Count >= historySize)
             {
-                historyTexts.Dequeue();
+                _historyTexts.Dequeue();
             }
 
-            historyTexts.Enqueue(text);
+            _historyTexts.Enqueue(text);
 
             DisplayDialogHistory();
         }
@@ -47,20 +64,84 @@ namespace Dialogs
         {
             string resultText = "";
 
-            for (int i = 0; i < historyTexts.Count; i++)
+            for (int i = 0; i < _historyTexts.Count; i++)
             {
-                string historyEntry = historyTexts.Dequeue();
+                string historyEntry = _historyTexts.Dequeue();
                 resultText += historyEntry + '\n';
-                historyTexts.Enqueue(historyEntry);
+                _historyTexts.Enqueue(historyEntry);
             }
 
             dialogHistory.text = resultText;
+        }
+
+        private void Update()
+        {
+            if (!_isTextConstructionInProgress)
+                return;
+
+            _textUpdater.Update();
+            newestDialogText.text = _newestActorLabel + _textUpdater.CurrentText;
         }
 
         private void Awake()
         {
             dialogHistory.text = "";
             newestDialogText.text = "";
+
+            _textUpdater = new SlowTextConstructor(timeBetweenLetters);
+            _textUpdater.onTextFinished += OnTextFinished;
+        }
+
+        private class SlowTextConstructor
+        {
+            public event Action onTextFinished;
+
+            private float _timePerLetter;
+            private float _timeSinceLastLetter;
+            private string _requestedText = "";
+
+            public SlowTextConstructor(float timePerLetter)
+            {
+                _timePerLetter = timePerLetter;
+            }
+
+            public string CurrentText { get; private set; } = "";
+
+            public void RequestText(string text)
+            {
+                _timeSinceLastLetter = 0;
+                _requestedText = text;
+                CurrentText = "";
+            }
+
+            public void Update()
+            {
+                if(CurrentText.Length == _requestedText.Length)
+                {
+                    onTextFinished?.Invoke();
+                    return;
+                }
+
+                while(_timeSinceLastLetter >= _timePerLetter)
+                {
+                    AddLetter();
+                    _timeSinceLastLetter -= _timePerLetter;
+
+                    if (CurrentText.Length == _requestedText.Length)
+                    {
+                        onTextFinished?.Invoke();
+                        return;
+                    }
+                }
+
+                _timeSinceLastLetter += Time.deltaTime;
+            }
+
+            private void AddLetter()
+            {
+                char nextLetter = _requestedText[CurrentText.Length];
+                CurrentText += nextLetter;
+            }
         }
     }
 }
--- Assets/Scripts/Dialogs/Execution/DialogRuntime.cs
+++ Assets/Scripts/Dialogs/Execution/DialogRuntime.cs
@@ -16,13 +16,13 @@ namespace Dialogs
         public void DisplayPlayerResponse(string response, Action onWorkDone)
         {
             Debug.Log("[Player] " + response);
-            responsesDisplayer.DisplayDialogText("[Player] " + response, onWorkDone);
+            responsesDisplayer.DisplayDialogText("Player", response, onWorkDone);
         }
 
         public void DisplayNpcResponse(string response, Action onWorkDone)
         {
             Debug.Log("[NPC] " + response);
-            responsesDisplayer.DisplayDialogText("[NPC] " + response, onWorkDone);
+            responsesDisplayer.DisplayDialogText("NPC", response, onWorkDone);
         }
 
         public void DisplayPlayerOptions(string[] options, Action<int> onOptionChosen)
